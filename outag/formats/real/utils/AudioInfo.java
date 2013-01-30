c79734b47e4bd2a66d863bfc26365ab09c8ea397
Jenua Boiko <jeyboy1985@gmail.com>
2013-01-30 02:54:38 +0200

    end with audio info

--- outag/formats/real/utils/AudioInfo.java
+++ outag/formats/real/utils/AudioInfo.java
@@ -4,7 +4,6 @@ import java.io.RandomAccessFile;
 
 import outag.formats.exceptions.InvalidSequenceException;
 import outag.formats.exceptions.UnsupportedException;
-import outag.formats.generic.GenericTag;
 import outag.formats.real.RealTag;
 
 public class AudioInfo {
@@ -60,6 +59,18 @@ public class AudioInfo {
 //	 #if version == 5
 //	  byte     Unknown
 //	 #endif
+	
+//	 #if version == 4
+//	 byte     Title string length
+//	 byte[]   Title string
+//	 byte     Author string length
+//	 byte[]   Author string
+//	 byte     Copyright string length
+//	 byte[]   Copyright string
+//	 byte     Comment string length
+//	 byte[]   Comment string
+//	 #endif	
+	
 //	 dword    Codec extradata length
 //	 byte[]   Codec extradata
 //	#endif	
@@ -102,52 +113,67 @@ public class AudioInfo {
 			if (!new String(buffer).equals("lpcJ"))
 				throw new InvalidSequenceException("AudioInfo sequence corrupted"); 
 		} else {
-//		#elseif version == 4 or version == 5
-//		 word     Unused (always 0)
-//		 byte[4]  ra signature (".ra4" or ".ra5", depending on version)
-//		 dword    possible data size - 0x27
-//		 word     Version2 (always equal to version)
-//		 dword    Header size - 16
-//		 word     Codec flavor
-//		 dword    Coded frame size
-//		 byte[12] Unknown
-//		 word     Sub packet h
-//		 word     Frame size
-//		 word     Subpacket size
-//		 word     Unknown
-//		 #if version == 5
-//		 	byte[6]  Unknown
-//		 #endif
-//		 word     Samplerate
-//		 word     Unknown
-//		 word     Sample size
-//		 word     Channels
-//		 #if version == 4
-//		  byte     Interleaver ID string length (always 4)
-//		  byte[]   Interleaver ID string
-//		  byte     FourCC string length (always 4)
-//		  byte[]   FourCC string
-//		 #endif
-//		 #if version == 5
-//		  dword    Interleaver ID
-//		  dword    FourCC
-//		 #endif
-//		 byte[3]  Unknown
-//		 #if version == 5
-//		  byte     Unknown
-//		 #endif
-//		 #if version == 4
-//			byte     Title string length
-//			byte[]   Title string
-//			byte     Author string length
-//			byte[]   Author string
-//			byte     Copyright string length
-//			byte[]   Copyright string
-//			byte     Comment string length
-//			byte[]   Comment string			
-//		 #end	
-//		 dword    Codec extradata length
-//		 byte[]   Codec extradata
+			codecInfo = new CodecInfo();
+			
+			f.readInt(); // Unused (always 0)
+			buffer = new byte[4]; f.read(buffer);
+//		 	byte[4]  ra signature (".ra4" or ".ra5", depending on version)
+			
+			long dataSize = f.readLong(); // possible data size - 0x27
+			f.readInt(); //  Version2 (always equal to version)
+			f.readLong();//  Header size - 16
+			
+			codecInfo.setcodecFlavor(f.readInt());
+			codecInfo.setcodecFrameSize(f.readLong());
+			buffer = new byte[12]; f.read(buffer); //Unknow
+
+			codecInfo.setsubpacketH(f.readInt());
+			codecInfo.setframeSize(f.readInt());
+			codecInfo.setsubpacketSize(f.readInt());
+			
+			f.readInt(); //   Unknown
+			
+			if (version == 5) {
+				buffer = new byte[6]; f.read(buffer); //Unknow
+			}
+				
+			codecInfo.setsampleRate(f.readInt());
+			f.readInt(); // Unknow
+			codecInfo.setsampleSize(f.readInt());
+			codecInfo.setchannels(f.readInt());
+
+			if (version == 4) {
+				buffer = new byte[f.read()]; f.read(buffer); //Interleaver ID string // always 4 bytes
+				buffer = new byte[f.read()]; f.read(buffer); //FourCC string // always 4 bytes
+			} else {
+				f.readLong(); // Interleaver ID / maybe need convert value to string 
+				f.readLong(); // FourCC // FourCC is four character constant / maybe need convert value to string 
+			}
+			
+			buffer = new byte[3]; f.read(buffer); //Unknow
+			if (version == 5)
+				f.read(); //Unknow
+
+//TODO: Check exist of next block for version 4 (maybe also include in 5 version)			
+			if (version == 4) {
+				tag = new RealTag();
+				
+				buffer = new byte[f.read()]; f.read(buffer);
+				tag.addTitle(new String(buffer));
+				
+				buffer = new byte[f.read()]; f.read(buffer);
+				tag.addArtist(new String(buffer));			
+
+				buffer = new byte[f.read()]; f.read(buffer);
+				tag.setCopyright(new String(buffer));			
+
+				buffer = new byte[f.read()]; f.read(buffer);
+				tag.addComment(new String(buffer));						
+			}
+
+//TODO: Check exist of next inclusions (maybe only for 5 version) 			 			
+			buffer = new byte[(int)f.readLong()]; f.read(buffer);
+			codecInfo.setcodecExtraData(buffer);
 		}			
 	}
 }
